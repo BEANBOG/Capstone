2d6778bfe964efa65b53710dec180b5d6a64ad7f
Janjuriki <jan.asajar@gmail.com>
2022-06-30 12:10:28 +0800

    press c to change character
    combine z and c to one key.

--- Assets/scripts/character swap/CharacterSwapNormal.cs
+++ Assets/scripts/character swap/CharacterSwapNormal.cs
@@ -6,17 +6,36 @@ public class CharacterSwapNormal : MonoBehaviour
 {
 
     public GameObject otherPlayer;
+    int active = 1;
+    int counter = 1;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            print("normal");
+            charSwap();
+            active = counter;
 
-
-            otherPlayer.GetComponent<DeafMovement>().enabled = false;
-            GetComponent<PlayerMovement>().enabled = true;
         }
     }
 
+    private void charSwap()
+    {
+
+
+        if (active == 1)
+        {
+            otherPlayer.GetComponent<DeafMovement>().enabled = false;
+            GetComponent<PlayerMovement>().enabled = true; ;
+            counter = 2;
+        }
+
+        if (active == 2)
+        {
+            otherPlayer.GetComponent<DeafMovement>().enabled = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            counter = 1;
+        }
+
+    }
 }
